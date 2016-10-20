6cb9dded7bf5bf01b235d2e953610251f17f1662
davide <davide.faconti@gmail.com>
2016-10-20 22:54:03 +0200

    Rebase from 'release/kinetic/ros_type_introspection'

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,3 +1,37 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright 2016 Davide Faconti
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of Willow Garage, Inc. nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+********************************************************************/
+
 #include <ros_type_introspection/parser.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/utility/string_ref.hpp>
@@ -267,14 +301,14 @@ ROSMessage::ROSMessage(const std::string &msg_def)
     }
 }
 
-void ROSMessage::updateTypes(std::vector<ROSType> all_types)
+void ROSMessage::updateTypes(const std::vector<ROSType> &all_types)
 {
     for (ROSField& field: _fields)
     {
         // if package name is missing, try to find msgName in the list of known_type
         if( field.type().pkgName().size() == 0 )
         {
-            for (ROSType& known_type: all_types)
+            for (const ROSType& known_type: all_types)
             {
                 if( field.type().msgName() == known_type.msgName() )
                 {
