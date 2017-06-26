16b4817c7c96b2b8ca930cda30024f6027b1b631
Davide Faconti <davide.faconti@gmail.com>
2017-06-26 11:24:31 +0200

    Rebase from 'rpm/kinetic/ros_type_introspection'

--- src/deserializer.cpp
+++ src/deserializer.cpp
@@ -206,49 +206,89 @@ void buildRosFlatType(const ROSTypeList& type_map,
 StringTreeLeaf::StringTreeLeaf(): node_ptr(nullptr), array_size(0)
 {  for (int i=0; i<7; i++) index_array[i] = 0;}
 
-SString StringTreeLeaf::toStr() const{
 
 
-  const StringTreeNode* node = this->node_ptr;
+// The idea comes from the talk by Alexandrescu
+// "Three Optimization Tips for C++".
 
-  if( !node ) return SString();
+// much faster for numbers below 100
+inline int print_number(char* buffer, uint16_t value)
+{
+    const char DIGITS[] =
+            "00010203040506070809"
+            "10111213141516171819"
+            "20212223242526272829"
+            "30313233343536373839"
+            "40414243444546474849"
+            "50515253545556575859"
+            "60616263646566676869"
+            "70717273747576777879"
+            "80818283848586878889"
+            "90919293949596979899";
+    if (value < 10)
+    {
+        buffer[0] = static_cast<char>('0' + value);
+        return 1;
+    }
+    else if (value < 100) {
+        value *= 2;
+        buffer[0] = DIGITS[ value+1 ];
+        buffer[1] = DIGITS[ value ];
+        return 2;
+    }
+    else{
+        return sprintf( buffer,"%d", value );
+    }
+}
 
-  const StringTreeNode* array[64];
+void StringTreeLeaf::toStr(SString& destination) const
+{
+
+  const StringTreeNode* leaf_node = this->node_ptr;
+
+  if( !leaf_node ) return destination.clear();
+
+  const StringTreeNode* nodes_from_leaf_to_root[64];
   int index = 0;
 
   int char_count = 0;
 
-  while(node)
+  while(leaf_node)
   {
-    char_count += node->value().size();
-    array[index] = node;
+    char_count += leaf_node->value().size();
+    nodes_from_leaf_to_root[index] = leaf_node;
     index++;
-    node = node->parent();
+    leaf_node = leaf_node->parent();
   };
 
-  array[index] = nullptr;
+  nodes_from_leaf_to_root[index] = nullptr;
   index--;
 
   int array_count = 0;
 
-  char buffer[200];
+  char buffer[256];
   int off = 0;
 
-  while ( index >=0)
+  while ( index >=0 )
   {
-    const SString& value =  array[index]->value();
+    const SString& value =  nodes_from_leaf_to_root[index]->value();
     if( value.size()== 1 && value.at(0) == '#' )
     {
       buffer[off-1] = '.';
-      off += sprintf( &buffer[off],"%d", this->index_array[ array_count++ ] );
+      off += print_number(&buffer[off], this->index_array[ array_count++ ] );
     }
     else{
-      off += sprintf( &buffer[off],"%s", array[index]->value().data() );
+      memcpy( &buffer[off], value.data(), value.size() );
+      off += value.size();
     }
-    if( index > 0 )  off += sprintf( &buffer[off],"/");
+    if( index > 0 ){
+        buffer[off] = '/';
+        off += 1;
+    }
     index--;
   }
-  return SString(buffer);
+  buffer[off] = '\0';
+  destination.assign(buffer, off);
 }
 
 
